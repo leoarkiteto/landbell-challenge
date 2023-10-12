77600d1c67fb49e00dfffe063224bf11d6e90ffd
Leo Franca <leoarkiteto@gmail.com>
2023-10-12 15:38:16 +0100

    :heavy_plus_sign: Add plugins to TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,vue}'
   ],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/typography"),
-  ],
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography')
+  ]
 }
