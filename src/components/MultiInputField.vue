5fe42d2e79b272b949de428992365285e456cd7e
Leo Franca <leoarkiteto@gmail.com>
2023-10-12 15:35:32 +0100

    :construction: Create Multi Input component

---
+++ src/components/MultiInputField.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+  interface Props {
+    type: "text" | "password" | "number" | "color" | "date";
+    text: string;
+    label?: string;
+    value: string;
+    nameField?: string;
+  }
+
+  defineEmits(["update:text", "update:value"]);
+  defineProps<Props>();
+</script>
+
+<template>
+  <label>{{ label }}</label>
+  <div class="flex gap-3">
+    <input
+      :name="nameField"
+      :type="type"
+      :value="text"
+      class="rounded-md border border-slate-300 px-3 py-2 placeholder-slate-400 focus:border-green-500 focus:outline-none focus:ring-1 focus:ring-green-500"
+      placeholder="Text"
+      v-bind="$attrs"
+      @input="$emit('update:text', $event.target?.value)"
+    />
+    <input
+      :name="nameField"
+      :type="type"
+      :value="value"
+      class="rounded-md border border-slate-300 px-3 py-2 placeholder-slate-400 focus:border-green-500 focus:outline-none focus:ring-1 focus:ring-green-500"
+      placeholder="Value"
+      v-bind="$attrs"
+      @input="$emit('update:value', $event.target?.value)"
+    />
+  </div>
+</template>
