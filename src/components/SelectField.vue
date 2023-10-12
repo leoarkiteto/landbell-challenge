a490270f92e8e8a4b67f817f7414d272a8c61e95
Leo Franca <leoarkiteto@gmail.com>
2023-10-12 21:06:56 +0100

    :bug: Fix bug about select value

--- src/components/SelectField.vue
+++ src/components/SelectField.vue
@@ -1,29 +1,36 @@
 <script lang="ts" setup>
-  import { ref, watch, watchEffect } from "vue";
+  import { computed, ref, watchEffect } from "vue";
+
+  interface Props {
+    options: {
+      value: string;
+      text: string;
+    }[];
+    modelValue: string;
+    label: string;
+  }
+
+  const props = defineProps<Props>();
 
   const emit = defineEmits(["update:modelValue"]);
-  const { modelValue, options, defaultText, label } = defineProps([
-    "modelValue",
-    "options",
-    "defaultText",
-    "label",
-  ]);
 
-  const selectedValue = ref(modelValue);
-
-  watchEffect(() => {
-    console.log(selectedValue.value);
-    emit("update:modelValue", selectedValue.value);
+  const value = computed({
+    get() {
+      return props.modelValue;
+    },
+    set(val) {
+      emit("update:modelValue", val);
+    },
   });
 </script>
 
 <template>
   <label for="inputItem">{{ label }}</label>
   <select
-    v-model="selectedValue"
+    v-model="value"
     class="rounded-md border border-slate-300 px-3 py-2 placeholder-slate-400 focus:border-green-500 focus:outline-none focus:ring-1 focus:ring-green-500"
   >
-    <option disabled value="">{{ defaultText }}</option>
+    <option disabled value="">Select an option</option>
     <option v-for="item in options" :key="item.value" :value="item.value">
       {{ item.text }}
     </option>
