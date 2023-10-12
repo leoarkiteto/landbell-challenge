ecaf4bc878da930a99fa3f159bbfc056871f9334
Leo Franca <leoarkiteto@gmail.com>
2023-10-12 15:40:06 +0100

    :construction: Implementation of logic

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <script lang="ts" setup>
-  import TheWelcome from "./components/TheWelcome.vue";
+  import { onMounted, ref } from "vue";
+
+  import FormDeck from "@/components/FormDeck.vue";
+  import FormBuilder from "@/components/FormBuilder.vue";
+  import { clearStorage, getStorage } from "@/utils/localData";
+
+  interface Field {
+    label: string;
+    name: string;
+    type: string;
+  }
+
+  const fieldToDeck = ref<Field[]>([]);
+
+  onMounted(() => {
+    if (getStorage("fields")) {
+      fieldToDeck.value = getStorage("fields");
+    }
+  });
+
+  function createDeck(data: Field) {
+    // fieldToDeck.value = [...fieldToDeck.value, data];
+    fieldToDeck.value.push(data);
+  }
+
+  function clearDeck() {
+    fieldToDeck.value = [];
+    clearStorage("fields");
+    clearStorage("fieldsData");
+  }
 </script>
 
-<template><TheWelcome /></template>
-
-<style scoped></style>
+<template>
+  <div class="flex h-screen items-center justify-center gap-12 bg-black p-8">
+    <FormDeck :fields="fieldToDeck" @clear="clearDeck" />
+    <FormBuilder @add-field="createDeck" />
+  </div>
+</template>
