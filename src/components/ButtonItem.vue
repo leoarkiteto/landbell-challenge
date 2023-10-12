7a130d93b457044eaadb308c3bb6d124247f0c73
Leo Franca <leoarkiteto@gmail.com>
2023-10-12 15:35:03 +0100

    :construction: Create Button component

---
+++ src/components/ButtonItem.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+  interface Props {
+    buttonType?: "submit" | "reset" | "button";
+  }
+
+  withDefaults(defineProps<Props>(), {
+    buttonType: "button",
+  });
+</script>
+
+<template>
+  <div
+    class="group flex w-fit cursor-pointer justify-center gap-1 rounded-md bg-green-500 px-4 py-2 text-white"
+  >
+    <button :type="buttonType" class="flex gap-2 group-hover:underline">
+      <slot />
+    </button>
+  </div>
+</template>
